917c70d7f6f77423e9dfa4c9af9f8f6132c64305
Sathnindu Kottage <45420836+sathninduk@users.noreply.github.com>
2021-09-11 12:13:00 +0530

    route initializing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
-import React, { Component } from "react";
+//import React, { Component } from "react";
+import React from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+// screens
+import indexComponent from './screens/index'
+
 function App() {
     return (
-        <div>
-            Hello to mernBase!
-        </div>
+        <Router>
+            <Route path="/" component={indexComponent} exact={true}/>
+        </Router>
     );
 }
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    //useQuery,
+    gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4000',
+    cache: new InMemoryCache()
+});
+
+client
+    .query({
+        query: gql`
+        {
+          todos {
+            id
+            text
+            complete
+          }
+        }
+    `
+    })
+    .then(result => console.log(result));
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ApolloProvider client={client}>
+            <App/>
+        </ApolloProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
